3d4427985f3c3378f015157250f19f24731c4e13
iamKhang <iamhoangkhang@icloud.com>
2024-09-13 08:00:41 +0700

    Week04 UseState CountArray

--- Week04/App.js
+++ Week04/App.js
@@ -4,12 +4,12 @@ import { Text, SafeAreaView, StyleSheet } from 'react-native';
 import { Card } from 'react-native-paper';
 
 // or any files within the Snack
-import CountApp from './components/01_countApp';
+import countArray from './components/02_countArray';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-        <CountApp />
+        <countArray />
     </SafeAreaView>
   );
 }
